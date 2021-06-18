f8b8891053375ef2990dea8fb89220dbd41bf02d
Jairo Perdomo <jairoperdomo09@gmail.com>
2021-06-17 23:35:30 -0600

    Spinner con valores definidos funcionando

--- app/src/main/java/com/example/pmiexamen201910110022/ActivityInsert.java
+++ app/src/main/java/com/example/pmiexamen201910110022/ActivityInsert.java
@@ -3,6 +3,7 @@ package com.example.pmiexamen201910110022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -12,12 +13,18 @@ import java.util.ArrayList;
 public class ActivityInsert extends AppCompatActivity {
     EditText name, phone, note;
     Spinner spinnerCountries;
-    ArrayList<String> arrayListCountries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+        spinnerCountries = findViewById(R.id.viewSpinnerCountries);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.array_countries, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerCountries.setAdapter(adapter);
 
         Button btnSave = findViewById(R.id.viewBtnSaveContact);
         spinnerCountries = findViewById(R.id.viewSpinnerCountries);
