b6e946933bc7207df97202f774098e59a27c70f7
Jairo Perdomo <jairoperdomo09@gmail.com>
2021-06-17 23:49:09 -0600

    Guardar contacto funcional

--- app/src/main/java/com/example/pmiexamen201910110022/ActivityInsert.java
+++ app/src/main/java/com/example/pmiexamen201910110022/ActivityInsert.java
@@ -2,28 +2,32 @@ package com.example.pmiexamen201910110022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.pmiexamen201910110022.transactions.Transactions;
 
 import java.util.ArrayList;
 
 public class ActivityInsert extends AppCompatActivity {
     EditText name, phone, note;
     Spinner spinnerCountries;
-
+    String country;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
         spinnerCountries = findViewById(R.id.viewSpinnerCountries);
-        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.array_countries, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
         spinnerCountries.setAdapter(adapter);
 
         Button btnSave = findViewById(R.id.viewBtnSaveContact);
@@ -31,5 +35,36 @@ public class ActivityInsert extends AppCompatActivity {
         name = findViewById(R.id.etName);
         phone = findViewById(R.id.etPhone);
         note = findViewById(R.id.etNote);
+        country = spinnerCountries.getSelectedItem().toString();
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addContact();
+            }
+        });
+    }
+
+    private void addContact() {
+        SQLiteConexion connection = new SQLiteConexion(this, Transactions.nameDataBase, null, 1);
+        SQLiteDatabase db = connection.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Transactions.country, country = spinnerCountries.getSelectedItem().toString());
+        values.put(Transactions.name, name.getText().toString());
+        values.put(Transactions.phone, phone.getText().toString());
+        values.put(Transactions.note, note.getText().toString());
+
+        Long result = db.insert(Transactions.tableContacts, Transactions.id, values);
+        Toast.makeText(getApplicationContext(),"Contacto guardado correctamente"+ result.toString(), Toast.LENGTH_LONG).show();
+        db.close();
+
+        ClearFields();
+    }
+
+    private void ClearFields() {
+        name.setText("");
+        phone.setText("");
+        note.setText("");
     }
 }
